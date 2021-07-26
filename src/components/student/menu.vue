7a269550009ede49ca642a03b4e69221a615befd
zhushanhuan <18815423036@163.com>
2021-07-26 22:49:47 +0800

    首页导航栏操作部分实现

--- src/components/student/menu.vue
+++ src/components/student/menu.vue
@@ -14,10 +14,12 @@
 							<a-icon type="user" /><span> 个人信息</span>
 						</template>
 						<a-menu-item key="1">
-							操作 1
+							<!-- 操作 1 -->
+							<router-link to="/indexs/userList">操作 1</router-link>
 						</a-menu-item>
 						<a-menu-item key="2">
-							操作 2
+							<!-- 操作 2 -->
+							<router-link to="">操作 2</router-link>
 						</a-menu-item>
 					</a-menu-item-group>
 					<a-menu-item-group key="g2">
@@ -25,10 +27,12 @@
 							<a-icon type="solution" /><span> 班级信息</span>
 						</template>
 						<a-menu-item key="3">
-							操作 3
+							<!-- 操作 3 -->
+							<router-link to="">操作 3</router-link>
 						</a-menu-item>
 						<a-menu-item key="4">
-							操作 4
+							<!-- 操作 4 -->
+							<router-link to="">操作 4</router-link>
 						</a-menu-item>
 					</a-menu-item-group>
 				</a-sub-menu>
@@ -36,116 +40,26 @@
 		</a-layout-sider>
 		<!-- 右边的内容 -->
 		<a-layout-content style="padding:15px 15px 15px 75px;">
-			<Welcome></Welcome>
-			<!-- 				<a-table :columns="columns" :data-source="data" :scroll="{ x: 1800, y: 300 }">
-					    <a slot="action" slot-scope="text">action</a>
-					  </a-table> -->
-			<!-- <slot></slot> -->
+			<!-- 路由匹配到的组件将渲染在这里 -->
+			<router-view></router-view>
+			<!-- <Welcome></Welcome> -->
 		</a-layout-content>
 	</a-layout>
 </template>
 
 <script>
 	import Welcome from '@/components/Welcome.vue'
-
-	const columns = [{
-			title: 'Full Name',
-			width: 100,
-			dataIndex: 'name',
-			key: 'name',
-			fixed: 'left'
-		},
-		{
-			title: 'Age',
-			width: 100,
-			dataIndex: 'age',
-			key: 'age',
-			fixed: 'left'
-		},
-		{
-			title: 'Column 1',
-			dataIndex: 'address',
-			key: '1',
-			width: 150
-		},
-		{
-			title: 'Column 2',
-			dataIndex: 'address',
-			key: '2',
-			width: 150
-		},
-		{
-			title: 'Column 3',
-			dataIndex: 'address',
-			key: '3',
-			width: 150
-		},
-		{
-			title: 'Column 4',
-			dataIndex: 'address',
-			key: '4',
-			width: 150
-		},
-		{
-			title: 'Column 5',
-			dataIndex: 'address',
-			key: '5',
-			width: 150
-		},
-		{
-			title: 'Column 6',
-			dataIndex: 'address',
-			key: '6',
-			width: 150
-		},
-		{
-			title: 'Column 7',
-			dataIndex: 'address',
-			key: '7',
-			width: 150
-		},
-		{
-			title: 'Column 8',
-			dataIndex: 'address',
-			key: '8',
-			width: 150
-		},
-		{
-			title: 'Column 9',
-			dataIndex: 'address',
-			key: '9'
-		},
-		{
-			title: 'Action',
-			key: 'operation',
-			fixed: 'right',
-			width: 100,
-			scopedSlots: {
-				customRender: 'action'
-			},
-		},
-	];
-
-	const data = [];
-	for (let i = 0; i < 100; i++) {
-		data.push({
-			key: i,
-			name: `Edrward ${i}`,
-			age: 32,
-			address: `London Park no. ${i}`,
-		});
-	}
+	import UserList from '@/components/student/UserList.vue'
 	export default {
 		data() {
 			return {
-				current: ['mail'],
+				// current: ['mail'],
 				openKeys: ['sub1'],
-				data,
-				columns
 			}
 		},
 		components: {
 			Welcome,
+			UserList,
 		},
 		watch: {
 			openKeys(val) {
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
 import Indexs from '../views/student/indexs.vue'
+import UserList from '../components/student/UserList.vue'
+import Welcome from '../components/Welcome.vue'
 Vue.use(VueRouter)
 
 //解决重复重定向问题
@@ -19,7 +21,11 @@ const routes = [
   {
     path: '/indexs',
     name: '学生首页',
-    component: Indexs
+    component: Indexs,
+	children:[	//子路由,嵌套路由
+		{path:'welcome',name: '欢迎页面',component: Welcome},
+		{path:'userlist',name: '操作1',component: UserList}
+	]
   },
   
 ]
