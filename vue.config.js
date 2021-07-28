0956d1ea90a7282a72503fa84780a820eecae620
zhushanhuan <1170047780@qq.com>
2021-07-28 17:56:50 +0800

    新增老师与管理员页面，找回密码页面还没有写完，登录页面还需完善

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  runtimeCompiler: true
+	runtimeCompiler: true,
+	devServer: {
+		port: 9091, //设置本地默认端口
+		proxy: { //设置代理
+			'/api': { //设置拦截器
+				target: 'http://localhost:9090', //代理的目标地址
+				changeOrigin: true, //是否设置同源，输入是的
+				pathRewrite: { //路径重写
+					'/api': '' //选择忽略拦截器里面的单词
+				}
+			}
+		}
+	}
 }
